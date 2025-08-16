18b24fc4a14bce292c2e86b7e5a44e672d2284f3
Aidar Khabirov <khabirov.ar1990@gmail.com>
2025-08-16 20:56:07 +0500

    8 bytes

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const FILENAME = "messages.txt"
 
 func main() {
-  fmt.Println("I hope I get the job!")
+	file, err := os.Open(FILENAME)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer file.Close()
+
+  buf := make([]byte, 8)
+
+  for {
+    count, err := file.Read(buf) 
+    if err == io.EOF {
+      break
+    }
+    fmt.Printf("read: %v\n", string(buf[:count]))
+  }
 }
